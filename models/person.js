43167bc5e5bdb423ad7b113b72cf7724cdb0ed18
Niklas Koskela <niklas.koskela@aalto.fi>
2023-10-07 17:11:50 +0300

    3.21

--- models/person.js
+++ models/person.js
@@ -16,7 +16,17 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{5,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Phone number required']
+  }
 })
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
